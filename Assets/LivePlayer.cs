9654d7f6b70368a3970c81c2f2d7720bb388c8ae
francisco-eduqo <francisco@eduqo.com.br>
2020-09-05 10:49:37 -0300

    Updating project, files and unity version

---
+++ Assets/LivePlayer.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LivePlayer : MonoBehaviour
+{
+	#region Live keyboard
+	public float transpose = 0;  // transpose in semitones
+	private List<AudioSource> audioSources = new List<AudioSource>();
+	public AudioClip baseNote;
+
+	private void Start()
+	{
+        if (baseNote == null) {
+            Debug.LogError("There's no base note");
+            return;
+        }
+        // Creating audiosources for each note
+        for (int i = 0; i < 15; i++)
+        {
+            AudioSource audioSource = gameObject.AddComponent<AudioSource>();
+            audioSource.clip = baseNote;
+            audioSources.Add(audioSource);
+        }
+	}
+
+	void Update()
+	{
+		List<int> notes = new List<int>();
+		if (Input.GetKeyDown("a")) notes.Add(0);  // C
+		if (Input.GetKeyDown("w")) notes.Add(1);  // C#
+		if (Input.GetKeyDown("s")) notes.Add(2);  // D
+		if (Input.GetKeyDown("e")) notes.Add(3);  // D#
+		if (Input.GetKeyDown("d")) notes.Add(4);  // E
+		if (Input.GetKeyDown("f")) notes.Add(5);  // F
+		if (Input.GetKeyDown("t")) notes.Add(6);  // F#
+		if (Input.GetKeyDown("g")) notes.Add(7);  // G
+		if (Input.GetKeyDown("y")) notes.Add(8);  // G#
+		if (Input.GetKeyDown("h")) notes.Add(9);  // A
+		if (Input.GetKeyDown("u")) notes.Add(10); // A#
+		if (Input.GetKeyDown("j")) notes.Add(11); // B
+		if (Input.GetKeyDown("k")) notes.Add(12); // C
+		if (Input.GetKeyDown("o")) notes.Add(13); // C#
+		if (Input.GetKeyDown("l")) notes.Add(14); // D  
+
+        // if some key pressed...
+		if (notes.Count > 0)
+		{
+            foreach(int note in notes) {
+                AudioSource audioSource = audioSources[note];
+                audioSource.pitch = Mathf.Pow(2, (note + transpose) / 12.0f);
+                audioSource.Play();
+            }
+		}
+	}
+	#endregion
+}
--- Assets/_Scripts/AudioManager.cs
+++ Assets/_Scripts/AudioManager.cs
@@ -301,38 +301,6 @@ public class AudioManager : MonoBehaviour {
 	}
 	#endregion
 
-	#region Live keyboard
-	public float transpose = 0;  // transpose in semitones
-	private new AudioSource audio;
-
-	private void Start()
-	{
-		audio = gameObject.AddComponent<AudioSource>();
-		audio.clip = notes[0].clip;
-	}
-
-	void Update()
-	{
-
-		float note = -1; // invalid value to detect when note is pressed
-		if (Input.GetKeyDown("a")) note = 0;  // C
-		if (Input.GetKeyDown("s")) note = 2;  // D
-		if (Input.GetKeyDown("d")) note = 4;  // E
-		if (Input.GetKeyDown("f")) note = 5;  // F
-		if (Input.GetKeyDown("g")) note = 7;  // G
-		if (Input.GetKeyDown("h")) note = 9;  // A
-		if (Input.GetKeyDown("j")) note = 11; // B
-		if (Input.GetKeyDown("k")) note = 12; // C
-		if (Input.GetKeyDown("l")) note = 14; // D
-
-		if (note >= 0)
-		{ // if some key pressed...
-			audio.pitch = Mathf.Pow(2, (note + transpose) / 12.0f);
-			audio.Play();
-		}
-	}
-	#endregion
-
 }
 /* Prioridades:
  * - Colocar câmera para seguir OK
